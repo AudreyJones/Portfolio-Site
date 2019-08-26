7e604a3b6d8331f6883604df4f6ae970a23169e8
AudreyJones <audrey.eliz.jones@gmail.com>
2019-08-26 14:27:10 -0400

    Starting text formatting

--- portfolio-site/src/containers/ProjectsPage.js
+++ portfolio-site/src/containers/ProjectsPage.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Card, CardDeck } from 'react-bootstrap'
-
+import Logo from '../logo192.png'
 
 
 class ProjectsPage extends Component {
     render() {
         return(
             <div>
-                <h1>Projects Page</h1>
+                <h1>Projects</h1>
                     <CardDeck>
                     <Card style={{ width: '18vw' }}>
-                        <Card.Img variant="top" alt="Logo"/>
+                        <Card.Img variant="top" src={Logo} alt="Logo"/>
                         <Card.Body>
                             <Card.Title>Knitpickr</Card.Title>
                             <Card.Text>Keep track of all of your yarn craft projects and materials using this web app!
@@ -23,7 +23,7 @@ class ProjectsPage extends Component {
                     </Card>
 
                     <Card style={{ width: '18vw' }}>
-                        <Card.Img variant="top" alt="Logo"/>
+                        <Card.Img variant="top" src={Logo} alt="Logo"/>
                         <Card.Body>
                             <Card.Title>AdventureApp</Card.Title>
                             <Card.Text>Go on an adventure starring you as a hero! Join villains on adventures for different popular Franchises, including Harry Potter, Star Wars, and the Marvel Cinematic Universe!</Card.Text>
@@ -34,7 +34,7 @@ class ProjectsPage extends Component {
                     </Card>
 
                     <Card style={{ width: '18vw' }}>
-                        <Card.Img variant="top" alt="Logo"/>
+                        <Card.Img variant="top" src={Logo} alt="Logo"/>
                         <Card.Body>
                             <Card.Title>CoffeeShop App</Card.Title>
                             <Card.Text>Create customized coffee orders containing multiple specialty coffee drinks. Update, favorite, and delete orders to customize your preferences!</Card.Text>
@@ -45,7 +45,7 @@ class ProjectsPage extends Component {
                     </Card>
 
                     <Card style={{ width: '18vw' }}>
-                        <Card.Img variant="top" alt="Logo"/>
+                        <Card.Img variant="top" src={Logo} alt="Logo"/>
                         <Card.Body>
                             <Card.Title>Top-Selling Nintendo Games CLI Gem</Card.Title>
                             <Card.Text>Get up-to-date information from Nintendo’s top-selling games site! Access popular titles and important information, including: number of players, game description to inform your next purchase. Happy Gaming!</Card.Text>
