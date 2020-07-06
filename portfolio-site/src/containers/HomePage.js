a6ae7b2291bd2f48a1de4bc881116766c5d6d528
AudreyJones <audrey.eliz.jones@gmail.com>
2020-07-06 12:32:17 -0400

    Centered text over card image overlay and spacing between rows

--- portfolio-site/src/containers/HomePage.js
+++ portfolio-site/src/containers/HomePage.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { Jumbotron, Card, CardGroup, Row, Col } from "react-bootstrap";
+import {
+  Jumbotron,
+  Card,
+  CardGroup,
+  Button,
+  ButtonGroup,
+  Row,
+  Col,
+  Container,
+} from "react-bootstrap";
+import Logo from "../logo192.png";
 
 class HomePage extends Component {
   render() {
@@ -9,70 +19,76 @@ class HomePage extends Component {
           <h1>Hi, I'm Audrey Jones!</h1>
           <p>Scroll down to see my work</p>
         </Jumbotron>
-        <CardGroup>
-          <Row>
-            <Col sm>
-              <Card className="project">
-                <Card.Img
-                  src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
-                  alt="Card image"
-                />
-                <Card.ImgOverlay>
-                  <Card.Title>Knitpickr</Card.Title>
-                </Card.ImgOverlay>
-              </Card>
-            </Col>
-            <Col sm>
-              <Card className="project">
-                <Card.Img
-                  src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
-                  alt="Card image"
-                />
-                <Card.ImgOverlay>
-                  <Card.Title>Project</Card.Title>
-                </Card.ImgOverlay>
-              </Card>
-            </Col>
-            <Col sm>
-              <Card className="project">
-                <Card.Img
-                  src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
-                  alt="Card image"
-                />
-                <Card.ImgOverlay>
-                  <Card.Title>Project</Card.Title>
-                </Card.ImgOverlay>
-              </Card>
-            </Col>
-          </Row>
-          <Card className="project">
-            <Card.Img
-              src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
-              alt="Card image"
-            />
-            <Card.ImgOverlay>
-              <Card.Title>Project</Card.Title>
-            </Card.ImgOverlay>
-          </Card>
-          <Card className="project">
-            <Card.Img
-              src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
-              alt="Card image"
-            />
-            <Card.ImgOverlay>
-              <Card.Title>Project</Card.Title>
-            </Card.ImgOverlay>
-          </Card>
-          <Card className="project">
-            <Card.Img
-              src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Test.png"
-              alt="Card image"
-            />
-            <Card.ImgOverlay>
-              <Card.Title>Project</Card.Title>
-            </Card.ImgOverlay>
-          </Card>
-        </CardGroup>
+        {/* <Card style={{ width: "20vw" }}>
+          <Card.Img variant="top" src={Logo} alt="Logo" />
+          <Card.Body>
+            <Card.Title>Knitpickr</Card.Title>
+            <Card.Text>
+              Keep track of all of your yarn craft projects and materials using
+              this web app!
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            {/* <small className="text-muted">Last updated 3 mins ago</small> */}
+        {/* </Card.Footer> */}
+        {/* </Card> */}
+        <Container fluid>
+          <CardGroup>
+            <Row>
+              <Col sm>
+                <Card className="project">
+                  <Card.Img src={Logo} alt="Card image" />
+                  <Card.ImgOverlay>
+                    <Card.Title>Knitpickr</Card.Title>
+                  </Card.ImgOverlay>
+                </Card>
+              </Col>
+              <Col sm>
+                <Card className="project">
+                  <Card.Img src={Logo} alt="Card image" />
+                  <Card.ImgOverlay>
+                    <Card.Title>Project</Card.Title>
+                  </Card.ImgOverlay>
+                </Card>
+              </Col>
+              <Col sm>
+                <Card className="project">
+                  <Card.Img src={Logo} alt="Card image" />
+                  <Card.ImgOverlay>
+                    <Card.Title>Project</Card.Title>
+                  </Card.ImgOverlay>
+                </Card>
+              </Col>
+            </Row>
+            <Row> </Row>
+            <Row>
+              <Col>
+                <Card className="project">
+                  <Card.Img src={Logo} alt="Card image" />
+                  <Card.ImgOverlay>
+                    <Card.Title>Project</Card.Title>
+                  </Card.ImgOverlay>
+                </Card>
+              </Col>
+              <Col>
+                <Card className="project">
+                  <Card.Img src={Logo} alt="Card image" />
+                  <Card.ImgOverlay>
+                    <Card.Title>Project</Card.Title>
+                  </Card.ImgOverlay>
+                </Card>
+              </Col>
+              <Col>
+                <Card className="project">
+                  <Card.Img src={Logo} alt="Card image" />
+                  <Card.ImgOverlay>
+                    <Card.Title>Project</Card.Title>
+                  </Card.ImgOverlay>
+                </Card>
+              </Col>
+            </Row>
+          </CardGroup>
+        </Container>
       </div>
     );
   }
