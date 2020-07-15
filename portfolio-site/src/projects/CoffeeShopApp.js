fe04d19d3ce3b4514e880a64a07d33afc046c11f
AudreyJones <audrey.eliz.jones@gmail.com>
2020-07-15 12:11:05 -0400

    Added styled linked buttons to each project

--- portfolio-site/src/projects/CoffeeShopApp.js
+++ portfolio-site/src/projects/CoffeeShopApp.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Image, Breadcrumb, Container, Button } from "react-bootstrap";
+import {
+  Image,
+  Breadcrumb,
+  Container,
+  Button,
+  Col,
+  Row,
+} from "react-bootstrap";
 import CoffeeShopAppPic from "../images/CoffeeShop.jpg";
 
 class CoffeeShopApp extends Component {
@@ -13,12 +20,21 @@ class CoffeeShopApp extends Component {
           <Breadcrumb.Item active>CoffeeShopApp</Breadcrumb.Item>
         </Breadcrumb>
         <Image src={CoffeeShopAppPic} fluid></Image>
-        <Button href="https://github.com/AudreyJones/CoffeeShop.git">
-          Github
-        </Button>{" "}
-        <Button href="https://drive.google.com/file/d/1tNpKa5NaT7ZWhRPoYpiRcfcMqQux8AfD/view?usp=sharing">
-          Demo
-        </Button>{" "}
+        <Row>
+          <Button
+            href="https://github.com/AudreyJones/CoffeeShop.git"
+            size="lg"
+            class="project-link"
+          >
+            Github
+          </Button>{" "}
+          <Button
+            href="https://drive.google.com/file/d/1tNpKa5NaT7ZWhRPoYpiRcfcMqQux8AfD/view?usp=sharing"
+            size="lg"
+          >
+            Demo
+          </Button>{" "}
+        </Row>
         <p>
           A Rails web app that enables users to create customized coffee orders
           containing multiple specialty coffee drinks. Users can update,
